b3729e41f64b61f11d0cc3cf290a97e819b246e0
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2020-03-08 19:16:58 +0300

    options

--- conftest.py
+++ conftest.py
@@ -3,32 +3,33 @@ from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption("--url", action="store", help="opencart url")
-    parser.addoption("--browser", action="store", help="browser for tests")
+    parser.addoption("--url", action="store", help="opencart url", default='http://localhost')
+    parser.addoption("--browser", action="store", help="browser for tests", choices=["chrome", "firefox", 'ie'], default='chrome')
+    parser.addoption("--platform", action="store", help="platform for tests", choices=["windows", "linux"], default='windows')
+    parser.addoption("--executor", action="store", help="executor for tests", choices=["local", "grid"], default='local')
 
 
 @pytest.fixture
 def browser(request):
-    cl_browser = request.config.getoption('--browser')
-    if cl_browser == 'ie':
-        wd = webdriver.Ie()
-    elif cl_browser == 'firefox':
-        options = webdriver.FirefoxOptions()
-        options.headless = True
-        wd = webdriver.Firefox(options=options)
+    cl_browser = request.config.getoption("--browser")
+    executor = request.config.getoption("--executor")
+    if executor == 'grid':
+        pass
     else:
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        wd = webdriver.Chrome(options=options)
-
+        if cl_browser == 'ie':
+            wd = webdriver.Ie()
+        elif cl_browser == 'firefox':
+            options = webdriver.FirefoxOptions()
+            options.headless = True
+            wd = webdriver.Firefox(options=options)
+        elif cl_browser == 'chrome':
+            options = webdriver.ChromeOptions()
+            options.add_argument('headless')
+            wd = webdriver.Chrome(options=options)
     request.addfinalizer(wd.quit)
     return wd
 
 
 @pytest.fixture
 def base_url(request):
-    cl_base_url = request.config.getoption('--url')
-    if cl_base_url == None:
-        return 'http://localhost'
-    else:
-        return cl_base_url
+    return request.config.getoption('--url')
