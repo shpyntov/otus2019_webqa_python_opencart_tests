3018bcf8b81d0e2a39c5de4371232dff44a70e0f
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2020-03-09 12:45:28 +0300

    tests fix

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,10 @@
 from pages import ProductPage
 
 
-def test_main_page(browser, base_url):
+def test_check_elements(browser, base_url):
     browser.get(base_url + '/index.php?route=product/product&product_id=43')
-    browser.find_elements_by_css_selector(ProductPage.PRODUCT_NAME)
-    browser.find_elements_by_css_selector(ProductPage.PRODUCT_PRICE)
-    browser.find_elements_by_css_selector(ProductPage.QUANTITY)
-    browser.find_elements_by_css_selector(ProductPage.ADDTOCART_BUTTON)
-    browser.find_elements_by_css_selector(ProductPage.BRAND_LINK)
+    browser.find_element_by_css_selector(ProductPage.PRODUCT_NAME)
+    browser.find_element_by_css_selector(ProductPage.PRODUCT_PRICE)
+    browser.find_element_by_css_selector(ProductPage.QUANTITY)
+    browser.find_element_by_css_selector(ProductPage.ADDTOCART_BUTTON)
+    browser.find_element_by_css_selector(ProductPage.BRAND_LINK)
--- test_search_page.py
+++ test_search_page.py
@@ -1,10 +1,10 @@
 from pages import SearchPage
 
 
-def test_main_page(browser, base_url):
+def test_check_elements(browser, base_url):
     browser.get(base_url + '/index.php?route=product/search')
-    browser.find_elements_by_css_selector(SearchPage.SEARCH_INPUT)
-    browser.find_elements_by_css_selector(SearchPage.CATEGOTY_SELECT)
-    browser.find_elements_by_css_selector(SearchPage.SEARCH_BUTTON)
-    browser.find_elements_by_css_selector(SearchPage.IN_CATEGORIES_CHECKBOX)
-    browser.find_elements_by_css_selector(SearchPage.IN_DESC_CHECKBOX)
+    browser.find_element_by_css_selector(SearchPage.SEARCH_INPUT)
+    browser.find_element_by_css_selector(SearchPage.CATEGOTY_SELECT)
+    browser.find_element_by_css_selector(SearchPage.SEARCH_BUTTON)
+    browser.find_element_by_css_selector(SearchPage.IN_CATEGORIES_CHECKBOX)
+    browser.find_element_by_css_selector(SearchPage.IN_DESC_CHECKBOX)
