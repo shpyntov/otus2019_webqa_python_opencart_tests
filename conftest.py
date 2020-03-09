219109324d63e09ace82d3dc0bd890b2dd5db3c4
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2020-03-09 12:21:13 +0300

    grid

--- conftest.py
+++ conftest.py
@@ -12,19 +12,21 @@ def pytest_addoption(parser):
 @pytest.fixture
 def browser(request):
     cl_browser = request.config.getoption("--browser")
-    executor = request.config.getoption("--executor")
-    if executor == 'grid':
-        pass
+    cl_executor = request.config.getoption("--executor")
+    if cl_executor == 'grid':
+        cl_platform = request.config.getoption("--platform")
+        wd = webdriver.Remote(command_executor="http://192.168.241.1:4444/wd/hub",
+                              desired_capabilities={"browserName": cl_browser, "platform": cl_platform})
     else:
         if cl_browser == 'ie':
             wd = webdriver.Ie()
         elif cl_browser == 'firefox':
             options = webdriver.FirefoxOptions()
-            options.headless = True
+            # options.headless = True
             wd = webdriver.Firefox(options=options)
         elif cl_browser == 'chrome':
             options = webdriver.ChromeOptions()
-            options.add_argument('headless')
+            # options.add_argument('headless')
             wd = webdriver.Chrome(options=options)
     request.addfinalizer(wd.quit)
     return wd
