59d3a79247c52243141e653d3832829752180514
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2020-03-09 20:39:17 +0300

    browserstack

--- conftest.py
+++ conftest.py
@@ -3,20 +3,33 @@ from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption("--url", action="store", help="opencart url", default='http://localhost')
-    parser.addoption("--browser", action="store", help="browser for tests", choices=["chrome", "firefox", 'ie'], default='chrome')
-    parser.addoption("--platform", action="store", help="platform for tests", choices=["windows", "linux"], default='windows')
-    parser.addoption("--executor", action="store", help="executor for tests", choices=["local", "grid"], default='local')
+    parser.addoption('--url', action='store', help='opencart url', default='http://localhost')
+    parser.addoption('--browser', action='store', help='browser for tests', choices=['chrome', 'firefox', 'ie'], default='chrome')
+    parser.addoption('--platform', action='store', help='platform for tests', choices=['windows', 'linux'], default='windows')
+    parser.addoption('--executor', action='store', help='executor for tests', choices=['local', 'grid', 'cloud'], default='local')
 
 
 @pytest.fixture
 def browser(request):
-    cl_browser = request.config.getoption("--browser")
-    cl_executor = request.config.getoption("--executor")
+    cl_browser = request.config.getoption('--browser')
+    cl_executor = request.config.getoption('--executor')
     if cl_executor == 'grid':
-        cl_platform = request.config.getoption("--platform")
-        wd = webdriver.Remote(command_executor="http://192.168.241.1:4444/wd/hub",
-                              desired_capabilities={"browserName": cl_browser, "platform": cl_platform})
+        cl_platform = request.config.getoption('--platform')
+        wd = webdriver.Remote(command_executor='http://192.168.241.1:4444/wd/hub',
+                              desired_capabilities={'browserName': cl_browser, 'platform': cl_platform})
+    elif cl_executor == 'cloud':
+        desired_cap = {
+            'browser': 'Firefox',
+            'browser_version': '74.0 beta',
+            'os': 'Windows',
+            'os_version': '7',
+            'resolution': '1024x768',
+            'name': 'Demo OpenCart Test'
+        }
+
+        wd = webdriver.Remote(
+            command_executor='https://yurysh1:m4RXgUbtwsbpHSxazUL3@hub-cloud.browserstack.com/wd/hub',
+            desired_capabilities=desired_cap)
     else:
         if cl_browser == 'ie':
             wd = webdriver.Ie()
