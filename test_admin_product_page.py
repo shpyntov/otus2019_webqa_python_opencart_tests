dc9cc38b6d8aa982783eba1af370b51ec5b844a9
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2020-04-05 18:10:09 +0300

    comments

--- test_admin_product_page.py
+++ test_admin_product_page.py
@@ -7,24 +7,25 @@ from random import randint
 
 
 def test_add_new_product(browser, base_url, random_product_name):
+    # preparing
     wait = WebDriverWait(browser, browser.wait)
     browser.get(base_url + '/admin')
-
+    # login to admin page
     browser.find_element_by_css_selector(AdminLoginPage.LOGIN_INPUT).send_keys('admin')
     browser.find_element_by_css_selector(AdminLoginPage.PASS_INPUT).send_keys('admin')
     browser.find_element_by_css_selector(AdminLoginPage.LOGIN_BUTTON).click()
-
+    # goto a products page
     browser.find_element_by_css_selector(AdminPage.CATALOG_MENU_BUTTON).click()
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, AdminPage.PRODUCTS_MENU_BUTTON)))
     browser.find_element_by_css_selector(AdminPage.PRODUCTS_MENU_BUTTON).click()
-
+    # add a new random product
     browser.find_element_by_css_selector(AdminPage.ADD_NEW_PRODUCT).click()
     browser.find_element_by_css_selector(AdminPage.NEW_PRODUCT_NAME).send_keys(random_product_name)
     browser.find_element_by_css_selector(AdminPage.NEW_PRODUCT_META).send_keys(random_product_name)
     browser.find_element_by_css_selector(AdminPage.DATA_TAB).click()
     browser.find_element_by_css_selector(AdminPage.NEW_PRODUCT_MODEL).send_keys(randint(1, 100))
     browser.find_element_by_css_selector(AdminPage.SAVE_NEW_PRODUCT).click()
-
+    # check that the new product in the table
     while True:
         products = browser.find_elements_by_css_selector(AdminPage.PRODUCT_NAME_IN_TABLE)
         for product in products:
