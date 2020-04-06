5ef661a4431a1afed97abf65dc2ff38b5f1130e7
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2020-04-06 16:39:41 +0300

    edit product test and locators fix

--- test_admin_product_page.py
+++ test_admin_product_page.py
@@ -20,12 +20,12 @@ def test_add_new_product(browser, base_url, random_product_name):
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, AdminPage.PRODUCTS_MENU_BUTTON)))
     browser.find_element_by_css_selector(AdminPage.PRODUCTS_MENU_BUTTON).click()
     # add a new random product
-    browser.find_element_by_css_selector(AdminPage.ADD_NEW_PRODUCT).click()
-    browser.find_element_by_css_selector(AdminPage.NEW_PRODUCT_NAME).send_keys(random_product_name)
-    browser.find_element_by_css_selector(AdminPage.NEW_PRODUCT_META).send_keys(random_product_name)
+    browser.find_element_by_css_selector(AdminPage.ADD_PRODUCT).click()
+    browser.find_element_by_css_selector(AdminPage.PRODUCT_NAME).send_keys(random_product_name)
+    browser.find_element_by_css_selector(AdminPage.PRODUCT_META).send_keys(random_product_name)
     browser.find_element_by_css_selector(AdminPage.DATA_TAB).click()
-    browser.find_element_by_css_selector(AdminPage.NEW_PRODUCT_MODEL).send_keys(randint(1, 100))
-    browser.find_element_by_css_selector(AdminPage.SAVE_NEW_PRODUCT).click()
+    browser.find_element_by_css_selector(AdminPage.PRODUCT_MODEL).send_keys(randint(1, 100))
+    browser.find_element_by_css_selector(AdminPage.SAVE_PRODUCT).click()
     # check that the new product in the table
     while True:
         products = browser.find_elements_by_css_selector(AdminPage.PRODUCT_NAMES_IN_TABLE)
@@ -38,7 +38,7 @@ def test_add_new_product(browser, base_url, random_product_name):
             browser.find_element_by_link_text('>').click()
 
 
-def test_remove_new_product(browser, base_url):
+def test_remove_product(browser, base_url):
     # preparing
     wait = WebDriverWait(browser, browser.wait)
     browser.get(base_url + '/admin')
@@ -55,7 +55,7 @@ def test_remove_new_product(browser, base_url):
     removed_product = products.pop(randint(0, len(products)-1))
     removed_product_name = removed_product.find_element_by_css_selector(AdminPage.PRODUCT_NAMES_IN_TABLE).text
     removed_product.find_element_by_css_selector(AdminPage.PRODUCT_CHECKBOXES_IN_TABLE).click()
-    browser.find_element_by_css_selector(AdminPage.REMOVE_NEW_PRODUCT).click()
+    browser.find_element_by_css_selector(AdminPage.REMOVE_PRODUCT).click()
     try:
         wait.until(EC.alert_is_present())
         alert = browser.switch_to.alert
@@ -73,3 +73,35 @@ def test_remove_new_product(browser, base_url):
                 return True
             else:
                 browser.find_element_by_link_text('>').click()
+
+
+def test_edit_product(browser, base_url, random_product_name):
+    # preparing
+    wait = WebDriverWait(browser, browser.wait)
+    browser.get(base_url + '/admin')
+    # login to admin page
+    browser.find_element_by_css_selector(AdminLoginPage.LOGIN_INPUT).send_keys('admin')
+    browser.find_element_by_css_selector(AdminLoginPage.PASS_INPUT).send_keys('admin')
+    browser.find_element_by_css_selector(AdminLoginPage.LOGIN_BUTTON).click()
+    # goto a products page
+    browser.find_element_by_css_selector(AdminPage.CATALOG_MENU_BUTTON).click()
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, AdminPage.PRODUCTS_MENU_BUTTON)))
+    browser.find_element_by_css_selector(AdminPage.PRODUCTS_MENU_BUTTON).click()
+    # edit random product
+    products = browser.find_elements_by_css_selector(AdminPage.PRODUCT_ROWS_IN_TABLE)
+    edited_product = products.pop(randint(0, len(products)-1))
+    edited_product.find_element_by_css_selector(AdminPage.EDIT_BUTTONS_IN_TABLE).click()
+    browser.find_element_by_css_selector(AdminPage.PRODUCT_NAME).clear()
+    browser.find_element_by_css_selector(AdminPage.PRODUCT_NAME).send_keys(random_product_name)
+    browser.find_element_by_css_selector(AdminPage.SAVE_PRODUCT).click()
+    assert AdminPage.SUCCESS_EDIT_ALERT_TEXT in wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, AdminPage.ALERT))).text
+    # check that the new product in the table
+    while True:
+        products = browser.find_elements_by_css_selector(AdminPage.PRODUCT_NAMES_IN_TABLE)
+        for product in products:
+            if random_product_name == product.text:
+                return True
+        if browser.find_elements_by_link_text('>') == []:
+            return False
+        else:
+            browser.find_element_by_link_text('>').click()
