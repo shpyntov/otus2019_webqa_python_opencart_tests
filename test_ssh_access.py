51c7b08d94fb1a25bd8f25f066f4e9819b9e95ff
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2020-04-18 21:27:47 +0300

    req update

--- test_ssh_access.py
+++ test_ssh_access.py
@@ -1,5 +1,5 @@
-def test_reboot(ssh_client, random_product_name):
-    ssh_client.exec_command('echo {} > test'.format(random_product_name))
-    stdin, stdout, stderr = ssh_client.exec_command('cat test')
-    data = (stdout.read() + stderr.read()).decode().strip()
-    assert data == random_product_name
+# def test_reboot(ssh_client, random_product_name):
+#     ssh_client.exec_command('echo {} > test'.format(random_product_name))
+#     stdin, stdout, stderr = ssh_client.exec_command('cat test')
+#     data = (stdout.read() + stderr.read()).decode().strip()
+#     assert data == random_product_name
